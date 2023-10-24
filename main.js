404e59bb3d5e4f6bec23cbc207c55ab17c665250
andrewscott <andrewscott2@usf.edu>
2023-10-24 19:43:41 -0400

    Add JS file and src

---
+++ main.js
@@ -0,0 +1,5 @@
+function showimage(){
+    var img = document.createElement("img");
+    img.src = "https://footballstadiumdigest.com/wp-content/uploads/2017/01/USF-Bulls.png"
+    document.getElementById("image").appendChild(img);
+}
