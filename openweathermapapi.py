97a217f4b5d2a483bcd6c93c60976dd2b4a3c682
andrewscott <andrewscott2@usf.edu>
2023-10-25 22:30:44 -0400

    API calls

---
+++ openweathermapapi.py
@@ -0,0 +1,22 @@
+#https://openweathermap.org/forecast5#geo5
+
+import requests
+import json
+
+key = "51de08d7fc9bfbc15136270d5738855f"
+
+data = {"lat": -39.809,
+        "lon": -16.787,
+        "model": "gfs",
+        "parameters": ["wind"],
+        "key": key
+        }
+
+header = {"Content-Type" :"application/json"}
+
+url = "https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid="
+print(url+key)
+r = requests.get(url+key)
+print(r.status_code)
+data = r.json()
+print(json.dumps(data,indent=2))
--- testapi.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-
-r = requests.get("https://data.wa.gov/api/views/f6w7-q2d2/rows.json?accessType=DOWNLOAD")
-data = r.text
