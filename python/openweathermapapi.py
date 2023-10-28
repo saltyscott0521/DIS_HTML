7979c583102fc9d8440fbcf9b249efe87fd68935
andrewscott <andrewscott2@usf.edu>
2023-10-28 14:53:45 -0400

    Adding py API test scripts

--- python/openweathermapapi.py
+++ python/openweathermapapi.py
@@ -2,6 +2,7 @@
 
 import requests
 import json
+import pandas as pd
 
 key = "51de08d7fc9bfbc15136270d5738855f"
 
@@ -19,4 +20,9 @@ print(url+key)
 r = requests.get(url+key)
 print(r.status_code)
 data = r.json()
+
 print(json.dumps(data,indent=2))
+
+f = open("./data/data.json","w")
+f.write(json.dumps(data))
+f.close
